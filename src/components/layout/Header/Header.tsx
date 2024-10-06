b0ad0933afc6202db17d70e3fd26d67786593a29
chrisrabe <chris.rabe14@gmail.com>
2024-10-07 07:59:49 +0800

    feat: created landing page

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,5 +1,24 @@
+import { Code } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import Image from "next/image";
 import type { FC } from "react";
 
-const Header: FC = () => <header>Header</header>;
+const Header: FC = () => (
+  <header className="px-80 py-4 flex justify-between items-center backdrop-blur-sm">
+    <Image
+      src="assets/logo-redot-with-text.svg"
+      alt="Redot Engine Logo"
+      width={120}
+      height={30}
+    />
+    <Button
+      variant="contained"
+      endIcon={<Code />}
+      className="bg-gray-800 capitalize"
+    >
+      Contribute
+    </Button>
+  </header>
+);
 
 export default Header;
--- src/components/sections/Landing/Landing.tsx
+++ src/components/sections/Landing/Landing.tsx
@@ -1,11 +1,38 @@
 import Header from "@/components/layout/Header";
+import { ArrowForward } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import Image from "next/image";
 import type { FC } from "react";
 
 const Landing: FC = () => {
   return (
-    <section>
+    <section className="flex flex-col">
       <Header />
-      Landing
+      <div className="w-full h-full flex flex-grow">
+        <div className="px-80 py-60 space-y-4">
+          <h1 className="text-6xl font-bold">
+            Your free, open-source game engine.
+          </h1>
+          <p>
+            Develop your 2D & 3D games, cross platform projects, or even XR
+            ideas!
+          </p>
+          <Button
+            variant="contained"
+            endIcon={<ArrowForward />}
+            className="bg-redot-primary capitalize px-8 py-4 text-lg"
+          >
+            Download
+          </Button>
+        </div>
+      </div>
+      <Image
+        src="/assets/hero-background.webp"
+        alt="hero-background"
+        fill
+        priority
+        className="-z-20 object-cover"
+      />
     </section>
   );
 };
